7ed6396b6d6c9b8dadf7568d21b97fd3a58b0c97
Simonas Hesthaven <91459433+Bob-Ross-Codes@users.noreply.github.com>
2024-11-05 22:06:42 +0100

    main light and intro cutscene

---
+++ Assets/2D Assets/Intro video/VideoSceneShift.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoSceneShift : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; // Reference to the VideoPlayer component
+    public string sceneToLoad; // Name of the scene to load after the video ends
+
+    void Start()
+    {
+        // Make sure we have a reference to the VideoPlayer component
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+
+        // Register for the video end event
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    // This function will be called when the video finishes playing
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    void OnDestroy()
+    {
+        // Unregister the event when the object is destroyed
+        videoPlayer.loopPointReached -= OnVideoEnd;
+    }
+}
