3296b3930a13b0b63a0311072b2c6d65fbc3ff71
Simonas Hesthaven <91459433+Bob-Ross-Codes@users.noreply.github.com>
2024-11-05 19:01:55 +0100

    Update RayCastAtSight.cs

--- Assets/Scripts/MainPlayer/RayCastAtSight.cs
+++ Assets/Scripts/MainPlayer/RayCastAtSight.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class RayCastAtSight : MonoBehaviour
 {
-    // public Gaze gazeScript; // Reference to the Gaze script
+    //slet(//)       public Gaze gazeScript; // Reference to the Gaze script
     public Camera mainCamera; // Reference to the main camera
 
     // Update is called once per frame
     void Update()
     {
-    //     if (gazeScript != null && mainCamera != null)
+    //slet(//)      if (gazeScript != null && mainCamera != null)
         {
             // Get the gaze location
-    //         Vector2 gazeLocation = gazeScript.gazeLocation;
+    //slet(//)          Vector2 gazeLocation = gazeScript.gazeLocation;
 
             // Convert gaze location to a ray
-    //         Ray ray = mainCamera.ScreenPointToRay(new Vector3(gazeLocation.x, gazeLocation.y, 0));
+    //slet(//)          Ray ray = mainCamera.ScreenPointToRay(new Vector3(gazeLocation.x, gazeLocation.y, 0));
 
             // Perform the raycast
-    //         if (Physics.Raycast(ray, out RaycastHit hit))
+    //slet(//)         if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 // Log the hit object
-   //              Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+   //slet(//)               Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
 
                 // Optionally, you can do something with the hit object
                 // For example, highlight it, interact with it, etc.
             }
 
             // Draw the ray in the scene view for debugging
-            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+    //slet(//)          Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
         }
     }
 }
