2b40473f6dcbb15316a520c423260320c6c00372
EmilieSommer <emilie.freltoft@gmail.com>
2024-11-15 14:13:32 +0100

    Scripts + shaders

--- Assets/Scripts/Hallway 2/DissolveObject.cs
+++ Assets/Scripts/Hallway 2/DissolveObject.cs
@@ -1,32 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
 public class DissolveObject : MonoBehaviour
 {
-    [SerializeField] private float noiseStrength = 0.25f;
-    [SerializeField] private float objectHeight = 1.0f;
+    public float _CutoffHeight = 0f;      // Controls height for dissolve
+    public float _NoiseScale = 1f;       // Controls the size of the noise
+    public float _NoiseStrength = 0.5f;  // Controls the intensity of the noise
+    public float dissolveSpeed = 0.1f;   // Speed of the dissolve effect
 
-    private Material material;
+    private bool isDissolving = false;
 
-    private void Awake()
+    void Update()
     {
-        material = GetComponent<Renderer>().material;
+        if (isDissolving)
+        {
+            // Incrementally dissolve
+            _CutoffHeight += Time.deltaTime * dissolveSpeed;
+            Shader.SetGlobalFloat("_CutoffHeight", _CutoffHeight);
+
+            // Stop when fully dissolved
+            if (_CutoffHeight >= 1.0f)
+            {
+                isDissolving = false;
+            }
+        }
+
+        // Always update noise parameters
+        Shader.SetGlobalFloat("_NoiseScale", _NoiseScale);
+        Shader.SetGlobalFloat("_NoiseStrength", _NoiseStrength);
     }
 
-    private void Update()
+    public void StartDissolve()
     {
-        var time = Time.time * Mathf.PI * 0.25f;
-
-        float height = transform.position.y;
-        height += Mathf.Sin(time) * (objectHeight / 2.0f);
-        SetHeight(height);
-    }
-
-    private void SetHeight(float height)
-    {
-        material.SetFloat("_CutoffHeight", height);
-        material.SetFloat("_NoiseStrength", noiseStrength);
+        isDissolving = true;
+        _CutoffHeight = 0f; // Reset dissolve effect
     }
 }
--- Assets/Scripts/Hallway 2/DissolveTrigger.cs
+++ Assets/Scripts/Hallway 2/DissolveTrigger.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DissolveTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public DissolveObject dissolveObject;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            dissolveObject.StartDissolve();
+        }
     }
 }
