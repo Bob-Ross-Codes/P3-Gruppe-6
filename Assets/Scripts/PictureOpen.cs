180969208e17a4b72eaa5b8d97980fe3b6d6232c
EmilieSommer <emilie.freltoft@gmail.com>
2024-11-18 12:25:00 +0100

    Handflip

--- Assets/Scripts/PictureOpen.cs
+++ Assets/Scripts/PictureOpen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PictureOpen : MonoBehaviour
-{
+/*{
     public GameObject picture;
     private bool isPictureActive = false;
     private bool isPictureRotated = false;
@@ -32,4 +32,57 @@ public class PictureOpen : MonoBehaviour
         }
 
     }
+}*/
+
+{
+    public Animator animator;
+    private bool isHandIn = false;        // Tracks if the hand is in
+    private bool isHandFlipped = false;  // Tracks if the hand is flipped
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isHandIn)
+            {
+                // Trigger the HandIn animation
+                animator.SetTrigger("HandIn");
+                isHandIn = true;
+            }
+            else if (isHandIn && !isHandFlipped)
+            {
+                // Trigger the HandFlip animation
+                animator.SetTrigger("HandFlip");
+                Debug.Log ("Flip!!!");
+                isHandFlipped = true;
+            }
+            else if (isHandIn && isHandFlipped)
+            {
+                // Trigger the HandOut animation
+                animator.SetTrigger("HandOut");
+                 Debug.Log ("Out!!!");
+
+                // Reset the flags after HandOut
+                 StartCoroutine(WaitForHandOutAnimation());
+            }
+        }
+    }
+
+
+    // Coroutine to wait for the HandOut animation to finish
+    private System.Collections.IEnumerator WaitForHandOutAnimation()
+    {
+        // Wait for the HandOut animation length
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Reset the states
+        isHandIn = false;
+        isHandFlipped = false;
+
+        // Clear all triggers to avoid unexpected behavior
+        animator.ResetTrigger("HandIn");
+        animator.ResetTrigger("HandFlip");
+        animator.ResetTrigger("HandOut");
+    }
 }
+
