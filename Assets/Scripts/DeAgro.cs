2fd5782f52a95f6dc595c44e66b207bdfb0b49b8
Garioo <108141262+Garioo@users.noreply.github.com>
2024-11-20 09:38:32 +0100

    Stop monster sound

--- Assets/Scripts/DeAgro.cs
+++ Assets/Scripts/DeAgro.cs
@@ -71,6 +71,7 @@ public class DeAgro : GazeActivation
         if (monsterPrefab != null)
         {
             Destroy(monsterPrefab);
+            AkSoundEngine.PostEvent("Stop_Monster_Sounds", gameObject);
             closetHideScript.StartCoroutine(closetHideScript.WaitForMonsterToBeDestroyed());
         }
 
