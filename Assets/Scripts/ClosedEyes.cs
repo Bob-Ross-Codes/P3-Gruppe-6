315d8ce2b5024a3e5a1f3eb101ad0495b7f943b9
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-11-21 15:04:00 +0100

    Closed knocking

---
+++ Assets/Scripts/ClosedEyes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosedEyes : MonoBehaviour
+{
+
+    [SerializedField] private Gaze gaze;
+
+    // Måske vi skal sætte det her script på playeren således det altid er til stede?
+    void Update()
+    {
+
+        if (gaze._blinking)
+        {
+            // Marius gør din ting!
+
+            // Storede fede pølsepatter
+        }
+
+
+    }
+}
