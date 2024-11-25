4225b9c229efc77311f276dd2982b203ca9a7bbd
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-11-25 13:02:38 +0100

    Update ClosedEyes.cs

--- Assets/Scripts/ClosedEyes.cs
+++ Assets/Scripts/ClosedEyes.cs
@@ -8,20 +8,30 @@ public class ClosedEyes : MonoBehaviour
     [SerializeField] private Gaze gaze;
     [SerializeField] private float blinkOffSet;
     private float blinkTime;
-    
-   private void Update()
+
+    private bool blinking;
+    private void Update()
     {
         //Debug.Log(" is brother blinking "+ gaze._blinking);
         if (Input.GetKeyDown(KeyCode.B) || gaze._blinking)
         {
-            blinkTime += Time.deltaTime;
-            if (blinkTime >= blinkOffSet)
-                AkSoundEngine.PostEvent("Play_Eyes_Closed", gameObject);
-          //   Debug.Log("BlinkingSound");
+            if (blinking == false)
+            {
+                blinking = true;
 
+                blinkTime += Time.deltaTime;
+                if (blinkTime >= blinkOffSet)
+                    AkSoundEngine.PostEvent("Play_Eyes_Closed", gameObject);
+                //   Debug.Log("BlinkingSound");
+
+            }
+
+            else
+            {
+                AkSoundEngine.PostEvent("Stop_Eyes_Closed", gameObject);
+                blinkTime = 0;
+                blinking = false;
+            }
         }
-        else AkSoundEngine.PostEvent("Stop_Eyes_Closed", gameObject); blinkTime = 0;
-
-
     }
 }
