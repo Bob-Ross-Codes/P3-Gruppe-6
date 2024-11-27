7e8669dad2cba506be850e3f9a96c13fb9ddb863
Sigurd Jonas Reseke Juul <sjuul23@student.aau.dk>
2024-11-27 13:00:24 +0100

    Ending

--- Assets/Scripts/RunScene/MonsterChaseMvmnt.cs
+++ Assets/Scripts/RunScene/MonsterChaseMvmnt.cs
@@ -56,6 +56,7 @@ public class MonsterChaseMvmnt : MonoBehaviour
             jumpscareManager.TriggerJumpscare();
             Destroy(gameObject);
 
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Ending");
         }
     }
 
