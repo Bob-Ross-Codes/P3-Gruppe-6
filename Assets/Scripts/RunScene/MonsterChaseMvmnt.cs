f5819a6e174aa73a9b9d1861299c3e52a58e5a1f
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-11-27 12:26:58 +0100

    Update MonsterChaseMvmnt.cs

--- Assets/Scripts/RunScene/MonsterChaseMvmnt.cs
+++ Assets/Scripts/RunScene/MonsterChaseMvmnt.cs
@@ -56,6 +56,8 @@ public class MonsterChaseMvmnt : MonoBehaviour
             jumpscareManager.TriggerJumpscare();
             Destroy(gameObject);
 
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Ending");
+
         }
     }
 
